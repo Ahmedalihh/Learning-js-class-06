c226a94ab3230d2ef3713033522ea8ced522598a
Ahmedalihh <ahmedalibbb227@gmail.com>
2024-12-24 21:48:17 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,103 @@
+// console.log("hello class");
+
+// var userAge = +prompt("Enter your Age");
+// console.log(userAge);
+// if (userAge === 18) {
+
+//Gates opt
+//And => && opt  => true && true => true
+//And => && opt  => fasle && true => false
+//And => && opt  => true && false => false
+//And => && opt  => false && false => false
+// var age = 18;
+
+// var gender = "male";
+
+// var gender = "male";
+// var gender = "female";
+
+// if (gender == "male" || gender == "other") {
+//   console.log("allow");
+// } else {
+//   console.log("not allow");
+// }
+
+// if (age == 18 && gender == "male") {
+//   console.log("if condition");
+// } else {
+//   console.log("else condition");
+// }
+
+// if (true) {
+//   console.log("if condition");
+// } else {
+//   console.log("else condition");
+// }
+
+// || => OR opt =>  true || true => true
+// || => OR opt =>  false || true => true
+
+// || => OR opt =>  true || false => true
+
+// || => OR opt => false || false => false
+
+// if (userAge >= 18) {
+//   console.log("allow");
+// } else {
+//   console.log("not allow");
+// }
+// 0 => false
+// null => false
+// "" => false
+// NaN => false
+// undefined => false
+// false => false
+
+// 1 => true
+
+// marksheet
+
+// var htmlMarks = +prompt("Enter html marks");
+// var cssMarks = +prompt("Enter CSS marks");
+
+// var javaScriptMarks = +prompt("Enter javaScript marks");
+
+// var reactMarks = +prompt("Enter react marks");
+
+// var nodeMarks = +prompt("Enter node marks");
+
+// var total = htmlMarks + cssMarks + javaScriptMarks + reactMarks + nodeMarks;
+// console.log(total);
+// var per = (total / 500) * 100;
+// var per = 90;
+// console.log(per);
+// if (per >= 90 && per <= 100) {
+//   console.log("A+");
+// } else if (per >= 80 && per <= 89) {
+//   console.log("A");
+// } else if (per >= 70 && per <= 79) {
+//   console.log("B");
+// } else if (per >= 60 && per <= 69) {
+//   console.log("C");
+// } else if (per >= 0 && per <= 59) {
+//   console.log("ghar jao beta");
+// } else {
+//   console.log("invalid per");
+// }
+
+// 90 >100 =>A+
+// 80 > 89 => A
+// 70 > 79 => B
+// 60 > 69 => C
+// 0 > 59 => ghar jao beta
+
+// obj/total*100
+
+// var age = 17;
+// var gender = "male";
+
+// if ((age >= 18 || age <= 40) && gender == "male") {
+//   console.log("allow");
+// } else {
+//   console.log("not allow");
+// }
